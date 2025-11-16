c737da9e3159c23f7280efb9ee12a1e758669893
Mundapolis <100851314+VTom21@users.noreply.github.com>
2025-11-16 13:23:52 +0100

    Shopping cart UI sidebar

--- games_main.php
+++ games_main.php
@@ -36,6 +36,19 @@
             </svg>
         </button>
 
+        <div id="cartSidebar" class="cart-sidebar"ng-class="{'active': cartOpen}">
+            <div class="cart-header">
+                <h2>Your Cart</h2>
+                <button class="close-btn" onclick="closeCart()"ng-class="{'active': cartOpen}" ng-click="cartOpen = false">×</button>
+            </div>
+
+            <div class="cart-content">
+                <p>Your cart is empty.</p>
+            </div>
+        </div>
+
+        <div id="cartOverlay" class="overlay" onclick="closeCart()"ng-class="{'active': cartOpen}" ng-click="cartOpen = false"></div>
+
         <button class="wish-btn" ng-click="wishOpen = true">
             <svg xmlns="http://www.w3.org/2000/svg" fill="#37e6ec73" viewBox="0 0 24 24" stroke-width="0" width="20" height="20" stroke="currentColor">
                 <path stroke-linecap="round" stroke-linejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12Z" />
