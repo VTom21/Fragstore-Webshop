50374741bd6a4058b761fc29febe86184eb722fe
Mundapolis <100851314+VTom21@users.noreply.github.com>
2025-12-07 18:49:12 +0100

    Icons Added

--- cart_website/sum_main.php
+++ cart_website/sum_main.php
@@ -12,6 +12,7 @@ $cart_items = json_decode($cart_json, true);
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="sum_main.css">
+    <link rel="icon" type="image/x-icon" href="/icons/array.png">
     <title>Checkout</title>
 </head>
 
--- order_successful/success.php
+++ order_successful/success.php
@@ -18,6 +18,7 @@ $currency_local = $_GET["currency"] ?? "$";
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="success.css">
+    <link rel="icon" type="image/x-icon" href="/icons/array.png">
     <title>Order Summary</title>
 </head>
 <body>
