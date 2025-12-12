759db279285ef4107651ab149d96758d5c71dd98
Mundapolis <100851314+VTom21@users.noreply.github.com>
2025-12-12 22:27:41 +0100

    Snake Game Basic Functionalities Done

--- snake/snake.js
+++ snake/snake.js
@@ -1,40 +1,88 @@
 const cell = 20;
-const width = 400;
-const height = 400;
+const width = 800;
+const height = 800;
 
+let foodImg;
+let snakeImg;
 
+//Creating Snake class for storing its positions and Velocity
+class Snake {
+  constructor(x, y, VelocityX, VelocityY) {
+    this.x = x;
+    this.y = y;
+    this.VelocityX = VelocityX;
+    this.VelocityY = VelocityY;
 
-class Snake{
-    constructor(x,y,VelocityX,VelocityY){
-        this.x = x;
-        this.y = y;
-        this.VelocityX = VelocityX;
-        this.VelocityY = VelocityY;
+    // Fix: Use cell size for proper grid alignment
+    
+    this.foodX = Math.floor(Math.random() * (width / cell)) * cell;
+    this.foodY = Math.floor(Math.random() * (height / cell)) * cell;
 
-        this.update = function(){
+    this.update = function () {
+      //Moving
+      this.x = this.x + this.VelocityX * cell;
+      this.y = this.y + this.VelocityY * cell;
 
-            //Moving
+      //Constrain Logic
+      if (this.x >= width || this.y >= height || this.x < 0 || this.y < 0) {
+        this.x = 200;
+        this.y = 200;
+      }
 
-            this.x = this.x + this.VelocityX * cell;
-            this.y = this.y + this.VelocityY * cell;
+      if (this.x === this.foodX && this.y === this.foodY) {
+        this.foodX = Math.floor(Math.random() * (width / cell)) * cell;
+        this.foodY = Math.floor(Math.random() * (height / cell)) * cell;
+      }
+    };
 
-            //Constrain
+    this.show = function () {
+      snakeImg = createImg("assets/body.png");
+      snakeImg.hide();
+      foodImg = createImg("assets/food.png");
+      foodImg.hide();
+    };
+  }
+}
 
+let snake;
 
-        }
+function setup() {
+  createCanvas(width, height);
+  snake = new Snake(200, 200, 1, 0);
+  Controls(snake);
+  frameRate(10);
+}
 
-        this.show = function(){
+function draw() {
+  background("#45B649");
+  snake.update();
+  snake.show();
+  image(foodImg, snake.foodX, snake.foodY);
+  image(snakeImg, snake.x, snake.y);
+}
 
-        }
+function Controls(snake) {
+  document.addEventListener("keydown", function (e) {
+    switch (e.key) {
+      case "ArrowUp":
+        snake.VelocityX = 0;
+        snake.VelocityY = -1;
+        break;
+
+      case "ArrowDown":
+        snake.VelocityX = 0;
+        snake.VelocityY = 1;
+        break;
+
+      case "ArrowLeft":
+        snake.VelocityY = 0;
+        snake.VelocityX = -1;
+        break;
+
+      case "ArrowRight":
+        snake.VelocityY = 0;
+        snake.VelocityX = 1;
+        break;
     }
-
-
-}
-
-function setup(){
-    createCanvas(width,height);
-}
-
-function draw(){
-    background(50);
+  });
 }
--- snake/snake.php
+++ snake/snake.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script src="https://cdnjs.cloudflare.com/ajax/libs/p5.js/2.0.5/p5.min.js" integrity="sha512-jOTg6ikYiHx1LvbSOucnvZi4shXbaovZ91+rfViIiUFLgAJK+k1oQtGEaLvDB8rsZwEfUksTgmhrxdvEDykuzQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/sketch.js/1.1/sketch.js" integrity="sha512-5t4lSiUS+fgaOiTm36s+DrDJc+rLx0zPHWrtbmnLsNOdjHyQLBg0mPpLoK2Qb6i5LpfRt6PdcG0FyiGmqVST0g==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    <script defer src="https://cdnjs.cloudflare.com/ajax/libs/alpinejs/3.12.0/cdn.min.js"></script>
     <script src="snake.js"></script>
     <title>Document</title>
 </head>
