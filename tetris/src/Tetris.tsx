13de872d5058329c64da84c9502e27b9d9522536
Mundapolis <100851314+VTom21@users.noreply.github.com>
2025-12-21 18:46:35 +0100

    Tetris Comments Done!

--- tetris/src/Tetris.tsx
+++ tetris/src/Tetris.tsx
@@ -90,26 +90,27 @@ function App() {
       });
     }
 
+
+    //Function for drawing out next random tetromino before first
     function Next_Tetromino(tetromino: typeof NextTetrominoRef.current) {
       if (!tetromino) return;
 
-      const sidebar = NextcanvasRef.current;
+      const sidebar = NextcanvasRef.current; //we set the sidebar variable to it's own canvas
       if (!sidebar) return;
 
-      const sidebar_ctx = sidebar.getContext("2d");
+      const sidebar_ctx = sidebar.getContext("2d"); //set the rendering to 2D
       if (!sidebar_ctx) return;
 
-      // CLEAR SIDEBAR CANVAS
-      sidebar_ctx.clearRect(0, 0, sidebar.width, sidebar.height);
+      sidebar_ctx.clearRect(0, 0, sidebar.width, sidebar.height); //perform initial clean on canvas
 
-      sidebar_ctx.fillStyle = getTetrominoColor(tetromino.type);
+      sidebar_ctx.fillStyle = getTetrominoColor(tetromino.type); //chooses the color that applies to the tetromino
 
-      tetromino.shape.forEach((row, y) => {
+      tetromino.shape.forEach((row, y) => { //we go through tetromino
         row.forEach((cell, x) => {
           if (cell) {
             sidebar_ctx.fillRect(
-              x * BLOCK_SIZE + 25,
-              y * BLOCK_SIZE + 20,
+              x * BLOCK_SIZE + 25, //defining x position
+              y * BLOCK_SIZE + 20, //defining y position
               BLOCK_SIZE,
               BLOCK_SIZE
             );
@@ -122,11 +123,7 @@ function App() {
 
     //Collision Checker Function
 
-    function CheckCollision(
-      tetro: typeof tetrominoRef.current,
-      offsetX = 0,
-      offsetY = 0
-    ) {
+    function CheckCollision(tetro: typeof tetrominoRef.current, offsetX = 0, offsetY = 0) {
       if (!tetro) return false;
 
       //Loops through rows and columns of Tetromino
@@ -146,9 +143,7 @@ function App() {
               //placed.shape[newY - placed.y] => checks if row exists
               //placed.shape[newY - placed.y][newX - placed.x] => checks if cell is filled
               // && binds the logic together
-              if (
-                placed.shape[newY - placed.y] &&
-                placed.shape[newY - placed.y][newX - placed.x]
+              if (placed.shape[newY - placed.y] && placed.shape[newY - placed.y][newX - placed.x]
               ) {
                 return true;
               }
@@ -175,27 +170,34 @@ function App() {
       return CheckCollision(tetro, 1, 0);
     }
 
+    //Edges Function for checking edge collision
     function Edges(tetro: typeof tetrominoRef.current) {
       if (!tetro) return { minC: 0, maxC: 0 };
-      const size = tetro.shape.length;
-      let minC = size;
+      const size = tetro.shape.length; //stores size of the tetromino array, exp. 3x3 => 3
+      let minC = size; //set this value as minimum column count by default
       let maxC = 0;
+
+      //By using minimum and maximum search, we get the smallest and biggest column a tetromino can access 
       for (let y = 0; y < size; y++) {
         for (let x = 0; x < size; x++) {
           if (tetro.shape[y][x]) {
-            minC = Math.min(minC, x);
-            maxC = Math.max(maxC, x);
+            minC = Math.min(minC, x); //Math.min() choose the smaller number inside the brackets
+            maxC = Math.max(maxC, x); //Math.max() choose the higher number inside the brackets
           }
         }
       }
 
-      return { minC, maxC };
+      return { minC, maxC }; //returns the smallest and biggest column
     }
 
+
+    //Clearlines function for clearing full rows
     function ClearLines() {
+      //we define two 2D arrays: for storing board content & tetromino colors
       const board: number[][] = [];
       const colors: string[][] = [];
 
+      //we fill up both arrays with zero and empty values
       for (let y = 0; y < ROWS; y++) {
         board[y] = [];
         colors[y] = [];
@@ -205,84 +207,102 @@ function App() {
         }
       }
 
+      //goes through tetrominos already placed on bottom
       placedTetrominos.current.forEach((tetro) => {
         const color = getTetrominoColor(tetro.type);
         tetro.shape.forEach((row, y) => {
           row.forEach((cell, x) => {
             if (cell) {
-              const gridY = tetro.y + y;
-              const gridX = tetro.x + x;
-              if (gridY >= 0 && gridY < ROWS && gridX >= 0 && gridX < COLS) {
-                board[gridY][gridX] = 1;
-                colors[gridY][gridX] = color;
+              const gridY = tetro.y + y; //store x coordinates
+              const gridX = tetro.x + x; //store y coordinates
+              if (gridY >= 0 && gridY < ROWS && gridX >= 0 && gridX < COLS) { //checks if tetromino is properly inside the grid
+                board[gridY][gridX] = 1; //sets a value of 1 inside new array at exact positions
+                colors[gridY][gridX] = color; //sets it's color accordingly
               }
             }
           });
         });
       });
 
-      const complete_lines: number[] = [];
+      const complete_lines: number[] = []; //we create a complete_lines array that contains all full rows
 
       for (let y = 0; y < ROWS; y++) {
-        if (board[y].every((cell) => cell == 1)) {
-          complete_lines.push(y);
+        if (board[y].every((cell) => cell == 1)) { //we filter out every cell that has the value of 1
+          complete_lines.push(y); //push it inside array
         }
       }
+      //we use setter to increase score by 10 at every full row
       setScore(prev => {
         const newScore = prev + 10 * complete_lines.length;
 
         if (scoreUI.current) {
-          scoreUI.current.textContent = `Score: ${newScore}`;
+          scoreUI.current.textContent = `Score: ${newScore}`; //Set the DOM value of current score (UI)
         }
 
-        const currentHigh = parseInt(high_score) || 0;
+        const currentHigh = parseInt(high_score) || 0; //store the high score in separate variable
 
-        if (newScore > currentHigh) {
+        if (newScore > currentHigh) { //condition to check if new score is higher than high score or not
 
           if (highScoreUI.current) {
-            highScoreUI.current.textContent = `High Score: ${newScore}`;
+            highScoreUI.current.textContent = `High Score: ${newScore}`; //Set the DOM value of high score (UI)
           }
 
-          localStorage.setItem("highScore", newScore.toString());
+          localStorage.setItem("highScore", newScore.toString()); //use localstorage for long-term high score saving 
         }
 
-        return newScore;
+        return newScore; //returns new current score
       });
 
 
+      //checks again if current new score is higher than the high score
       if (score > parseInt(high_score)) {
-        localStorage.setItem("highScore", score.toString());
+        localStorage.setItem("highScore", score.toString()); //then sets the highest using localStorage
       }
 
       if (complete_lines.length === 0) return;
 
+      //we go through all filled lines (top to bottom)
       for (let y = complete_lines.length - 1; y >= 0; y--) {
-        const line = complete_lines[y];
+        const line = complete_lines[y]; //store the row itself
 
+        //by using splice(), we remove one row at the index where our first full row is, then delete the colors tied to the shapes too
         board.splice(line, 1);
         colors.splice(line, 1);
 
+        //by using unshift(), we fill up the top of the array with a full row of zeros (empty spaces)
         board.unshift(Array(COLS).fill(0));
         colors.unshift(Array(COLS).fill(""));
+
+        //increase the index of every other row (makes sure multiple row removal can happen)
+
+        /*
+        exp. if line 0 gets removed, then we unshift so on top there's a row full of 0. 
+        Therefore line 1 will not have index 1 anymore but 0, so we need to increment indexes 
+        if we want to remove multiple at one go. (if possible)
+        
+        */
 
         for (let j = 0; j <= y; j++) {
           complete_lines[j]++;
         }
       }
 
+
+      //we empty out array that contains all tetrominos placed on bottom
       placedTetrominos.current = [];
       for (let y = 0; y < ROWS; y++) {
         for (let x = 0; x < COLS; x++) {
           if (board[y][x] === 1) {
-            const color = colors[y][x];
-            const typeIndex = SHAPE_COLORS.indexOf(color);
-            const type = Object.keys(SHAPES)[typeIndex];
+            const color = colors[y][x]; //stores color of tetromino, exp. blue
+            const typeIndex = SHAPE_COLORS.indexOf(color); //gets out the index of the tetromino's color, exp. 2
+            const type = Object.keys(SHAPES)[typeIndex]; //searches for tetromino type by indexing its shape, exp. shapes[2] = l
 
+            //pushes the block into the 2D array
             placedTetrominos.current.push({
-              shape: [[1]],
-              x: x,
-              y: y,
-              type: type,
+              shape: [[1]], //creating one block
+              x: x, //x-axis
+              y: y, //y-axis
+              type: type, //tetromino type, exp. i, j
             });
           }
         }
@@ -308,10 +328,12 @@ function App() {
       //We perform rotation
       //example of Matrix Rotation with T-shape (counter-clockwise):
       /* 
+
       0°  : (1,1)
       90° : (0,1)
       180°: (1,0)
       270°: (2,1)
+      360°: (1,1)
 
       */
       for (let y = 0; y < size; y++) {
@@ -319,22 +341,21 @@ function App() {
           const newX = size - 1 - y; //Gets the value of X
           const newY = x; //Y gets the value of X in previous angle
 
-
-          newElements[newY][newX] = tetromino.shape[y][x];
+          newElements[newY][newX] = tetromino.shape[y][x]; //stores filled values into the new array
         }
       }
 
       //applies the new position for the tetromino
       tetromino.shape = newElements;
 
-      const { minC, maxC } = Edges(tetromino);
+      const { minC, maxC } = Edges(tetromino); //extract minimum and maximum column count from Edges() class
 
-      if (tetromino.x + maxC >= COLS) {
-        tetromino.x -= tetromino.x + maxC - COLS + 1;
+      if (tetromino.x + maxC >= COLS) { //checks if the tetromino on the biggest column, is out of bounds or not on the right edge
+        tetromino.x -= tetromino.x + maxC - COLS + 1; //if yes, tetromino gets pushed back inside the grid
       }
 
-      if (tetromino.x + minC < 0) {
-        tetromino.x += -(tetromino.x + minC);
+      if (tetromino.x + minC < 0) { //checks if the tetromino on the biggest column, is out of bounds or not on the left edge
+        tetromino.x += -(tetromino.x + minC);  //if yes, tetromino gets pushed back inside the grid
       }
     }
 
