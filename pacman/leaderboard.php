1782edd9fc8b48e5248108c0109fa2102d40c580
Visegrádi Tamás <72553156301@szentgyorgyi.internal>
2026-02-16 10:00:36 +0100

    leaderboard deprecation error fix

--- pacman/leaderboard.php
+++ pacman/leaderboard.php
@@ -6,7 +6,6 @@ $ch = curl_init();
 curl_setopt($ch, CURLOPT_URL, $url);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 $response = curl_exec($ch);
-curl_close($ch);
 
 $data = json_decode($response, true);
 
